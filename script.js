ab125419871f13fe49ef1b026e52f452578f0584
EwuraAbena Antwi <ewuraabenaantwibosiako@gmail.com>
2025-05-05 19:56:51 +0000

    improve script and allow multi digit values to be evaluated

--- script.js
+++ script.js
@@ -22,60 +22,44 @@ function divide (someNumbers){
 }
 
 function operate(num1, operator, num2){
-  let sign = operator
-  switch (sign) {
+  switch (operator) {
     case "+": return add([num1, num2]);
     case "-": return subtract([num1, num2]);
-    case "x": return multiply([num1, num2]);
+    case "X": return multiply([num1, num2]);
     case "/": return divide([num1, num2]);
   }
 }
+let currentStatus = "num1";
 
-  
-buttons.forEach((button, index) => {
-  button.addEventListener("click", () => {
-    for (let i=1; i<=1; i++) {
-      if (index === 0){displaying.textContent = "0"}
-      else if (index === 1){displaying.textContent = "1";}
-      else if (index === 2){displaying.textContent = "2";}
-      else if (index === 3){displaying.textContent = "3";}
-      else if (index === 4){displaying.textContent = "4";}
-      else if (index === 5){displaying.textContent = "5";}
-      else if (index === 6){displaying.textContent = "6";}
-      else if (index === 7){displaying.textContent = "7";}
-      else if (index === 8){displaying.textContent = "8";}
-      else if (index === 9){displaying.textContent = "9";}
-      else if (index === 10){displaying.textContent = ".";}
-     
-      else if (index === 12){displaying.textContent = "+";}
-      else if (index === 13){displaying.textContent = "-";}
-      else if (index === 14){displaying.textContent = "x";}
-      else if (index === 15){displaying.textContent = "/";}
-    
-      count += 1;
+buttons.forEach((button) => {
+  button.addEventListener("click", ()=> {
+  let value = button.textContent;
+
+  if (!isNaN(value) || (value ===".")){displaying.textContent += value
+
+    if (currentStatus === "num1"){num1 = parseInt(displaying.textContent)}
+    else if (currentStatus === "num2"){
+      num2 = parseInt(displaying.textContent)
     }
-    
-    console.log(count);
-    if (count === 1){num1 = parseInt(displaying.textContent)}
-    else if(count === 2){operator = displaying.textContent}
-    else if (count === 3){num2 = parseInt(displaying.textContent)}
-
-    result = operate(num1, operator, num2);
-    
-    })
-  })
-  equals.addEventListener("click", () => {
-    displaying.textContent = result;
-    console.log("done");
-  })
-clear.addEventListener("click", ()=> {
-  displaying.textContent= "";
-  num1 = "";
-  operator="";
+  }
+  else if (["+", "-", "X", "/"].includes(value)){
+    operator = value;
+    currentStatus = "num2";
+    displaying.textContent = "";
+  }
+}
+)})
+equals.addEventListener("click", ()=> {
+  let result = operate(num1, operator, num2);
+  displaying.textContent = result;
+  num1 = result;
+  operator ="";
   num2 = "";
-  count = 0;
+});
+clear.addEventListener("click", ()=> {
+  displaying.textContent="";
+  num1 ="";
+  operator="";
+  num2="";
+  currentStatus = "num1";
 })
-
-
-
-
